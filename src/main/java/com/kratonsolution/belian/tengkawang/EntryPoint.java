c66ccc44ffa565bd77505682315a8672d148cf3a
Agung Dodi Perdana <agungdp@localhost.localdomain>
2020-11-25 05:31:55 +0700

    add dummy controller

--- src/main/java/com/kratonsolution/belian/tengkawang/EntryPoint.java
+++ src/main/java/com/kratonsolution/belian/tengkawang/EntryPoint.java
@@ -3,8 +3,12 @@ package com.kratonsolution.belian.tengkawang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Vector;
+
 @SpringBootApplication
 public class EntryPoint {
+	
+	public static Vector<String> memory = new Vector<>();
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EntryPoint.class, args);
