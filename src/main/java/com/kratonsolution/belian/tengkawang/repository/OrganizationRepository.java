70cf04b058bc68afe11bafe24b3c25e81baadc49
'agungdp' <'agung.dodi.perdana@gmail.com'>
2020-12-10 08:23:39 +0700

    add Company Structure module code

--- src/main/java/com/kratonsolution/belian/tengkawang/repository/OrganizationRepository.java
+++ src/main/java/com/kratonsolution/belian/tengkawang/repository/OrganizationRepository.java
@@ -1,8 +1,14 @@
 package com.kratonsolution.belian.tengkawang.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.kratonsolution.belian.tengkawang.model.Organization;
+
+import lombok.NonNull;
 
 /**
  * @author Agung Dodi Perdana
@@ -11,4 +17,8 @@ import com.kratonsolution.belian.tengkawang.model.Organization;
  */
 public interface OrganizationRepository extends JpaRepository<Organization, String> {
 
+	public Optional<Organization> findOneByName(@NonNull String name);
+	
+	@Query("SELECT org FROM Organization org WHERE org.name != ?0")
+	public List<Organization> findAllExcludeName(@NonNull String name);
 }
